4364c837c625dc2745c9a901ac7d7cb59fe9fc50
adyto <adiyulianto61@gmail.com>
2023-01-05 17:12:42 +0700

    navbar mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BsMoon, BsCoin } from 'react-icons/bs';
 import Select from 'react-select';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { MdLightMode } from 'react-icons/md';
 import { useStateContext } from '../context/StateContext';
 import { NumericFormat } from 'react-number-format';
@@ -19,25 +20,11 @@ const Navbar = () => {
     cryptoStat,
   } = useStateContext();
 
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <div className="flex flex-col lg:flex-col-reverse">
-      <div className="flex flex-row justify-between container mx-auto py-3">
-        <div className="flex flex-row items-center lg:gap-2">
-          <BsCoin className="lg:w-7 lg:h-7 text-green-500" />
-          <Link to={'/'} className="font-semibold lg:text-2xl">
-            Cryptoread
-          </Link>
-        </div>
-        <div className="flex flex-row lg:gap-8 items-center max-lg:hidden">
-          <div className="flex flex-row space-x-4">
-            <Link to="/">Home</Link>
-            <Link to={'/cryptocurrencies'}>Cryptocurrencies</Link>
-            <Link to={'/exchanges'}>Exchanges</Link>
-            <Link to={'/news'}>News</Link>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-row justify-between container mx-auto py-2">
+    <div className="bg-transparent flex flex-col w-full">
+      <div className="flex flex-row justify-between container mx-auto py-2 max-lg:hidden">
         <div className="flex flex-row space-x-4 max-lg:flex-wrap max-lg:justify-center max-lg:space-x-0 max-lg:gap-x-2 max-lg:mx-auto">
           <span className="text-xs font-semibold">
             Cryptos:{' '}
@@ -91,7 +78,7 @@ const Navbar = () => {
             />
           </span>
         </div>
-        <div className="flex flex-row max-lg:hidden">
+        <div className="flex flex-row">
           <Select
             onChange={handleChangeCurrency}
             options={optionsCurrency}
@@ -103,6 +90,47 @@ const Navbar = () => {
           <button onClick={toggleDarkMode}>
             {darkMode ? <MdLightMode /> : <BsMoon />}
           </button>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between container mx-auto items-center max-lg:hidden">
+        <div className="flex flex-row items-center">
+          <BsCoin className="w-7 h-7 text-green-500" />
+          <Link to={'/'} className="font-semibold text-2xl">
+            Cryptoread
+          </Link>
+        </div>
+        <div className="flex flex-row gap-8 items-center">
+          <div className="flex flex-row space-x-4">
+            <Link to="/">Home</Link>
+            <Link to={'/cryptocurrencies'}>Cryptocurrencies</Link>
+            <Link to={'/exchanges'}>Exchanges</Link>
+            <Link to={'/news'}>News</Link>
+          </div>
+        </div>
+      </div>
+      <div className="fixed inset-x-0 top-0 z-50 bg-white/25 backdrop-blur-sm border-b-2 py-4 border-white/20 lg:hidden ">
+        <div className="flex flex-row w-full justify-between">
+          <div className="flex flex-row items-center">
+            <BsCoin className="w-7 h-7 text-green-500" />
+            <Link to={'/'} className="font-semibold text-2xl">
+              Cryptoread
+            </Link>
+          </div>
+          <div className="flex mr-4">
+            <HiMenuAlt4
+              onClick={() => setToggle(true)}
+              className="w-9 h-9 text-black"
+            />
+            {toggle && (
+              <div className="fixed z-20 w-3/4 h-screen right-0 top-0 flex flex-col items-end justify-start bg-white py-4">
+                <HiX
+                  onClick={() => setToggle(false)}
+                  className="w-9 h-9 text-black mr-4"
+                />
+                <div className="flex flex-col  w-full">123</div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/container/HomePage/index.jsx
+++ src/container/HomePage/index.jsx
@@ -9,40 +9,42 @@ const HomePage = () => {
   const { currencySign, currencySymbol, cryptoStat } = useStateContext();
 
   return (
-    <div className="flex flex-col container mx-auto">
+    <>
       <Navbar />
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="text-2xl font-bold">
-          Today's Cryptocurrency Prices by Market Cap
-        </h1>
-        <h2 className="font-medium text-sm">
-          The global crypto market cap is{' '}
-          <span className="font-bold">
-            {`${currencySign !== `null` ? currencySign : currencySymbol}`}
-            {millify(cryptoStat?.totalMarketCap, {
-              units: ['', 'K', 'million', 'billion', 'T', 'P', 'E'],
-              space: true,
-              precision: 2,
-            })}
-          </span>
-        </h2>
+      <div className="flex flex-col container mt-20 lg:mt-0 mx-auto">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold">
+            Today's Cryptocurrency Prices by Market Cap
+          </h1>
+          <h2 className="font-medium text-sm">
+            The global crypto market cap is{' '}
+            <span className="font-bold">
+              {`${currencySign !== `null` ? currencySign : currencySymbol}`}
+              {millify(cryptoStat?.totalMarketCap, {
+                units: ['', 'K', 'million', 'billion', 'T', 'P', 'E'],
+                space: true,
+                precision: 2,
+              })}
+            </span>
+          </h2>
+        </div>
+        <BannerCoin />
+        <div className="flex justify-between items-center mt-10">
+          <h2>Top 10 Cryptocurrencies in the world</h2>
+          <h3>
+            <Link to={'/cryptocurrencies'}>Show more</Link>
+          </h3>
+        </div>
+        <CryptoCurrencies simplified />
+        <div className="flex justify-between items-center mt-10">
+          <h2>Latest Crypto News</h2>
+          <h3>
+            <Link to={'/news'}>Show more</Link>
+          </h3>
+        </div>
+        <News simplified />
       </div>
-      <BannerCoin />
-      <div className="flex justify-between items-center mt-10">
-        <h2>Top 10 Cryptocurrencies in the world</h2>
-        <h3>
-          <Link to={'/cryptocurrencies'}>Show more</Link>
-        </h3>
-      </div>
-      <CryptoCurrencies simplified />
-      <div className="flex justify-between items-center mt-10">
-        <h2>Latest Crypto News</h2>
-        <h3>
-          <Link to={'/news'}>Show more</Link>
-        </h3>
-      </div>
-      <News simplified />
-    </div>
+    </>
   );
 };
 
